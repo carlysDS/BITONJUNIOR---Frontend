e4135376d1ed45660b1a5d73881d45453a6968fc
Carlys De Sousa <carlysdesousa@gmail.com>
2026-03-25 11:37:36 +0100

    codigo modificado para conexion con AWS

--- lib/servicios/auth_servicio.dart
+++ lib/servicios/auth_servicio.dart
@@ -1,12 +1,13 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AuthService {
-  final String apiUrl = 'https://tu-api.com/api/auth';
+  final String apiUrl = dotenv.env['API_BASE_URL'] ?? 'http://localhost:8000/api';
 
   Future<Map<String, dynamic>> register(Map<String, dynamic> data) async {
     final response = await http.post(
-      Uri.parse('$apiUrl/register'),
+      Uri.parse('$apiUrl/auth/register'),
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode(data),
     );
