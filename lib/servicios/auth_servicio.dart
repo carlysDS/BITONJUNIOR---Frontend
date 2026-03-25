3d23b9ecf52ca5fd8834d4c0f505b7dcfe2d4b22
Carlys De Sousa <carlysdesousa@gmail.com>
2026-03-25 16:40:42 +0100

    Codigo con la conexión a Laravel exitosa

--- lib/servicios/auth_servicio.dart
+++ lib/servicios/auth_servicio.dart
@@ -3,24 +3,34 @@ import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AuthService {
-  final String apiUrl = dotenv.env['API_URL'] ?? 'http://18.100.79.40:8000/api/auth';
+  final String apiUrl = dotenv.env['API_URL'] ?? 'http://18.100.79.40:8000/api/auth/register';
 
   Future<Map<String, dynamic>> register(Map<String, dynamic> data) async {
+    print('Inicio del register');
     try{
+       final url = apiUrl;
+      print('🌍 URL: $url');
       final response = await http.post(
-        Uri.parse('$apiUrl/register'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode(data),
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',},
+        body: jsonEncode(data),      
     );
+    print('✅ STATUS: ${response.statusCode}');
+    print('📦 BODY: ${response.body}');
+
     return {
       'statusCode': response.statusCode,
       'body': jsonDecode(response.body),
     };
-    }catch(e){
-      return{
+    }catch (e) {
+       print('🔥 ERROR REAL: $e');
+
+    return {
       'statusCode': 500,
-        'body': {'message': 'Error de conexión'},
-      };
-    }
+      'body': {'message': e.toString()},
+  };
+  }
   }
 }
